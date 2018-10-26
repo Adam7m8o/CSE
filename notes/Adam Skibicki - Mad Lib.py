eaa0e5457b31d6e8dadb51f30bc822d1a49cb756
Adam7m8o <42383372+Adam7m8o@users.noreply.github.com>
2018-10-26 09:45:40 -0700

    finished
    finished

---
+++ notes/Adam Skibicki - Mad Lib.py
@@ -0,0 +1,18 @@
+# ten input words
+adj1 = input("type in adj1")
+adj2 = input("type in adj2")
+adj3 = input("type in adj3")
+vrb1 = input("type in vrb1")
+vrb2 = input("type in vrb2 past tense")
+vrb3 = input("type in vrb3 ending in ing")
+noun1 = input("type in noun1")
+noun2 = input("type in noun2")
+noun3 = input("type in noun3")
+name = input("type in name")
+
+# the actual mad lib
+print("Hello my name is "+name+"")
+print("My favorite place is a "+adj1+" "+noun1+"")
+print("I absolutely hate "+adj2+""+noun2+"")
+print("I love to "+vrb1+", with "+adj3+" dogs, while also "+vrb3+"")
+print("But that all changed when the "+noun3+" "+vrb2+"")
