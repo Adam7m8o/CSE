af38b027d86908bdf972354fb8b28684aad94def
Adam7m8o <42383372+Adam7m8o@users.noreply.github.com>
2019-01-08 08:44:16 -0800

    Work

---
+++ notes/Adam,Skibicki-Semester_2_Notes.py
@@ -0,0 +1,25 @@
+'''
+print("Hello World")
+#oof
+print("Meme Review")
+'''
+'''
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+'''
+'''
+age = input("how old are you?")
+print("Wwwwwwwwwwwwwaaaooooooowwww your " + age + " years old wwwwwwwwwwwwooooooooooooowwwww")
+'''
+
+list1 = ["blue, yellow, orange, green, red"]
+
+list1.append("brown")
+
+print(list1)
+
+list1.pop(0)
+
+print(list1)
