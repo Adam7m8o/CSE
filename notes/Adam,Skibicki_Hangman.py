68e0d9cc593d47ba9be7a814fa54ea86cfc954da
Adam7m8o <42383372+Adam7m8o@users.noreply.github.com>
2019-01-11 09:45:40 -0800

    yaaay

---
+++ notes/Adam,Skibicki_Hangman.py
@@ -0,0 +1,12 @@
+# import random
+import random
+
+# variables
+tries = 8
+
+# word bank
+word = ("Hello", "Goodbye", "Hollow Knight", "Flex Tape", "Thatsa lotta damage", "Lilo and stich",
+"Megalovania", "Undertale's Fanbase", "million", "Lion King")
+
+# random
+Word = random.choice(word)
