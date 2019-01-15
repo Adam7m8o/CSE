45f60c8670ec39b42b3cb7a39e9bd2f601d2255f
Adam7m8o <42383372+Adam7m8o@users.noreply.github.com>
2019-01-15 08:42:37 -0800

    oof

--- notes/Adam,Skibicki_Hangman.py
+++ notes/Adam,Skibicki_Hangman.py
@@ -3,10 +3,35 @@ import random
 
 # variables
 tries = 8
+guesses = []
+Win = False
 
 # word bank
 word = ("Hello", "Goodbye", "Hollow Knight", "Flex Tape", "Thatsa lotta damage", "Lilo and stich",
 "Megalovania", "Undertale's Fanbase", "million", "Lion King")
 
-# random
+# word choice
 Word = random.choice(word)
+
+# Make word list
+Word = list(Word)
+print(Word)
+
+# loop
+while tries > 0:
+    guesses = input("choose a letter")
+
+    tries -= 1
+    if tries == 0:
+        Win = False
+
+'''
+# win/lose
+if Win = True:
+    print("You win")
+
+if Win = False:
+    print("You lose")
+
+# For i in range(len(Word))
+'''
