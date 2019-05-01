60dc67e2d154fd5b9044d4dac7fdd72be33b8a04
Adam7m8o <42383372+Adam7m8o@users.noreply.github.com>
2019-05-01 09:40:58 -0700

    yes

--- notes/Adam S._Validator.py
+++ notes/Adam S._Validator.py
@@ -1,7 +1,7 @@
 import csv
 
 # Drop the last digit from the number. The last digit is what we want to check against
-# Reverse the numbers
+# Reverse the numbers - Done
 # Multiply the digits in odd positions (1, 3, 5, etc.) by 2 and subtract 9 to all any result higher than 9
 # Add all the numbers together
 # The check digit (the last number of the card) is the amount that you would need to add to get a multiple of 10
@@ -15,7 +15,7 @@ def check_how_long(num: str):
 
 def remove(num: str):
     if len(num) == 16:
-        print(num[:-1])
+        print(num[15])
 
 
 def reverse(num: str):
@@ -23,7 +23,10 @@ def reverse(num: str):
     print(num[::-1])
 
 
-# with open("book1.csv", "r") as old_csv:
+def multiply_odd(num: str):
+
+
+    # with open("book1.csv", "r") as old_csv:
 #     with open("MyNewishFile.csv", "w", newline='') as new_csv:
 #         reader = csv.reader(old_csv)
 #         writer = csv.writer(new_csv)
