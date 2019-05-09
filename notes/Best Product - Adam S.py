17532128b9e49e1c926440f6a48e5feacb05c847
Adam7m8o <42383372+Adam7m8o@users.noreply.github.com>
2019-05-09 08:45:17 -0700

    v.wegrtv jqw

--- notes/Best Product - Adam S.py
+++ notes/Best Product - Adam S.py
@@ -3,10 +3,11 @@ import csv
 with open("Sales") as csv_file:
     csv_reader = csv.reader(csv_file)
     for row in csv_reader:
-        if row[13] == 'Total Profit':
-            continue
-        profit = row[13]
-        print(float(profit))
+        if row[2] == "Fruits":
+            if row[13] == 'Total Profit':
+                continue
+            profit = row[13]
+            # print(float(profit))
 
-    for row in csv_reader:
-         = len(list(Sales))
+    # for row in csv_reader:
+    #     Thing = len(list("Sales"))
